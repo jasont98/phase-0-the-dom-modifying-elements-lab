ea83db5bc2d0b0c0e5ec63f7ad2c55002d9c96a5
Hoang Tran <jashoang2583@gmail.com>
2022-10-13 09:37:38 -0700

    commit tree 56287dd2bb07c08bee4bded1e857857c4bfe9d52

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById("main");
+// console.log(main);
+main.remove();
+
+const newHeader = document.createElement("h1"); 
+newHeader.id = "victory"; 
+newHeader.textContent = "Jason is the champion";
